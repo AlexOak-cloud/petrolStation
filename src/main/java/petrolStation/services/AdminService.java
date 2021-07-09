2b04cd9e542378cc6fd7eb0b4b77a5cac1348958
AlexOak <dubovec995@gmail.com>
2021-07-09 20:32:55 +0300

    create_tables

---
+++ src/main/java/petrolStation/services/AdminService.java
@@ -0,0 +1,14 @@
+package petrolStation.services;
+
+import petrolStation.model.Petrol;
+
+public class AdminService {
+
+    public static void createStation(String name, Petrol... petrol){
+    }
+
+
+
+
+
+}
